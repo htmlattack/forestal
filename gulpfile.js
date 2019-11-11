beaaff813d3bda4eaf965445aaeb0d45075eef43
alexandroleynik <alexandr@oleynik.org>
2019-11-11 03:01:19 +0200

    Main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,220 +1,185 @@
-"use strict";
-//===========================================
-var gulp = require('gulp'),
-	autoprefixer = require('gulp-autoprefixer'),
-	clean = require('gulp-clean'),
-	cleanCSS = require('gulp-clean-css'),
-	imagemin = require('gulp-imagemin'),
-	imageminPngquant = require('imagemin-pngquant'),
-	rename = require("gulp-rename"),
-	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps'),
-	concat = require('gulp-concat'),
-	order = require("gulp-order"),
-	uglify = require('gulp-uglify'),
-	spritesmith = require('gulp.spritesmith'),
-	browserSync = require('browser-sync'),
-	reload = browserSync.reload,
-	notify = require("gulp-notify"),
-	fileinclude = require('gulp-file-include');
-//===========================================
-var buidPath = 'build/',
-	path = {
-		build: {
-			home: buidPath,
-			html: buidPath + '**/*.html',
-			htmlHome: buidPath + '*.html',
-			htmlRigger: buidPath + 'html/*.html',
-			htmlRiggerFiles: buidPath + 'html/**/*.html',
-			php:  buidPath + '**/*.php',
-			js:   buidPath + 'js/**/*.js',
-			jsPath: buidPath + 'js/',
-			jsLib: buidPath + 'js/lib/**/*.js',
-			jsLibPath: buidPath + 'js/lib/',
-			sass: buidPath + 'sass/**/*.+(sass|scss)',
-			sassPath: buidPath + 'sass/',
-			css:  buidPath + 'css/**/*.css',
-			cssPath:  buidPath + 'css/',
-			icon: buidPath + 'img/icons/*.+(png|jpg)',
-			img:  buidPath + 'img/**/*.*',
-			imgPath:  buidPath + 'img/',
-			image:  buidPath + 'image/**/*.*',
-			imagePath:  buidPath + 'image/',
-			fonts:  buidPath + 'fonts/**/*.*'
-		},
-		dist: {
-			home: 'dist/',
-			js: 'dist/js/',
-			css: 'dist/css/',
-			img: 'dist/img/',
-			image: 'dist/image/',
-			fonts: 'dist/fonts/'
-		}
-	};
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var order = require('gulp-order');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var notify = require('gulp-notify');
+var fileinclude = require('gulp-file-include');
+var plumber = require('gulp-plumber');
+
+var srcPath = 'src/';
+var buildPath = 'build/';
+
+var	path = {
+	source: {
+		home: srcPath,
+		html: srcPath + '**/*.html',
+		htmlHome: srcPath + '*.html',
+		htmlRigger: srcPath + '*.html',
+		htmlRiggerFiles: srcPath + '**/*.html',
+		js:   srcPath + 'js/**/*.js',
+		jsPath: srcPath + 'js/',
+		jsLib: srcPath + 'js/libs/**/*.js',
+		jsLibPath: srcPath + 'js/libs/',
+		sass: srcPath + 'sass/*.+(sass|scss)',
+		sassPath: srcPath + 'sass/**/*.+(sass|scss)',
+	},
+
+	build: {
+		home: buildPath,
+		js: buildPath+'assets/js/',
+		css: buildPath+'assets/css/',
+		html: buildPath,
+	}
+};
+
+var onError = function(err) {
+    notify.onError({
+    	title: "Error in " + err.plugin,
+    	message: err.message
+    })(err);
+    this.emit('end');
+};
+
+
 // Server
-//===========================================
 gulp.task('openServer', function () {
 	browserSync({
-		proxy: 'js.loc/', // local server
+		proxy: 'js.loc/',
 		online: true,
 		host: 'localhost',
     port: 9000,
-		logPrefix: "openServer"
+		logPrefix: 'openServer'
 	});
 });
-//===========================================
+
 gulp.task('webServer', function () {
 	browserSync({
 		server: {
-			baseDir: "./build/"
+			baseDir: './build/',
 		},
 		online: true,
 		host: 'localhost',
-		port: 9000,
-		logPrefix: "webServer"
+		port: 777,
+		logPrefix: 'webServer'
 	});
 });
-// HTML/PHP
-// ===========================================
+
+
+// HTML
 gulp.task('html:build', function () {
-	gulp.src(path.build.htmlRigger)
-		.pipe(fileinclude())
+	gulp.src(path.source.htmlRigger)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file',
+		}))
 		.pipe(gulp.dest(path.build.home))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true
+		}));
 });
-// JavaScript
-// ===========================================
+
+
+// JS
 gulp.task('js:build', function(){
-	return gulp.src(path.build.jsLib)
+	return gulp.src(path.source.jsLib)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
 		.pipe(order([
-			'jquery.js',
-			path.build.jsLib
+			'jquery.min.js',
+			path.source.jsLib
 		]))
 		.pipe(sourcemaps.init())
-		.pipe(concat('scripts.js'))
-    .pipe(gulp.dest(path.build.jsPath))
+		.pipe(concat('app.js'))
+    	//.pipe(gulp.dest(path.build.js))
 		.pipe(uglify())
 		.pipe(rename(function (path) {
-			path.extname = ".min.js"
+			path.extname = '.min.js'
 		}))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.build.jsPath))
+		.pipe(gulp.dest(path.build.js))
 		.on('error', function(err) {
 			console.error('Error in compress task', err.toString());
 		});
 });
-gulp.task('jsMain:build', function(){
-	return gulp.src(path.build.jsPath + 'main.js')
-		.pipe(sourcemaps.init())
-		.pipe(uglify())
-		.pipe(rename(function (path) {
-			path.extname = ".min.js"
-		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.build.jsPath))
-		.on('error', function(err) {
-			console.error('Error in compress task', err.toString());
-		});
-});
+
+
 // SASS/SCSS
-// ===========================================
 gulp.task('sass:build', function () {
-	return gulp.src(path.build.sass)
-		.pipe(sourcemaps.init())
-		.pipe(sass().on("error", notify.onError()))
-		.pipe(autoprefixer(['last 20 versions', '> 1%', 'ie > 8']))
-		.pipe(rename(function (path) {
-			path.extname = ".css"
+	return gulp.src(path.source.sass)
+		.pipe(plumber({
+			errorHandler: onError
 		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.build.cssPath))
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(autoprefixer(['last 2 versions', '> 0.5%', 'IE 9-11']))
+		// .pipe(rename(function (path) {
+		// 	path.extname = '.css'
+		// }))
+		// .pipe(sourcemaps.write())
+		// .pipe(gulp.dest(path.build.css))
 		.pipe(cleanCSS())
 		.pipe(sourcemaps.write())
 		.pipe(rename(function (path) {
-			path.extname = ".min.css"
+			path.extname = '.min.css'
 		}))
-		.pipe(gulp.dest(path.build.cssPath))
+		.pipe(gulp.dest(path.build.css))
 		.pipe(reload({stream: true}));
 });
-// Img min / sprite
-// ===========================================
-gulp.task('sprite:build', function () {
-	var spriteData = gulp.src(path.build.icon)
-		.pipe(spritesmith({
-			imgName: 'sprite.png',
-			cssName: '_sprite.scss',
-			algorithm: 'diagonal',
-			// padding: 100,
-			imgPath: '../img/sprite.png'
-		}))
-	spriteData.img.pipe(gulp.dest(path.build.imgPath));
-	spriteData.css.pipe(gulp.dest(path.build.sassPath));
-});
+
+
 // Clean
-//===========================================
 gulp.task('clean:dist', function () {
-	return gulp.src(path.dist.home, {read: false})
-		.pipe(clean());
+	return gulp.src(path.build.home, {
+		read: false,
+	}).pipe(plumber({
+		errorHandler: onError
+	})).pipe(clean());
 });
+
+
 // Dist
-// //===========================================
 gulp.task('dist', ['clean:dist'], function () {
 	gulp.src(path.build.css)
-		.pipe(gulp.dest(path.dist.css))
-	gulp.src(path.build.fonts)
-		.pipe(gulp.dest(path.dist.fonts))
-	gulp.src(path.build.js)
-		.pipe(gulp.dest(path.dist.js))
-	gulp.src(path.build.html)
-		.pipe(gulp.dest(path.dist.home))
-	gulp.src(path.build.php)
-		.pipe(gulp.dest(path.dist.home));
-	gulp.src(path.build.img)
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [imageminPngquant()],
-			interlaced: true
+		.pipe(plumber({
+			errorHandler: onError
 		}))
-		.pipe(gulp.dest(path.dist.img))
-	gulp.src(path.build.image)
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [imageminPngquant()],
-			interlaced: true
+		.pipe(gulp.dest(path.build.css))
+	gulp.src(path.source.js)
+		.pipe(plumber({
+			errorHandler: onError
 		}))
-		.pipe(gulp.dest(path.dist.image))
+		.pipe(gulp.dest(path.build.js))
 });
+
 
 // Watch
-// ===========================================
 gulp.task('watch', function () {
-	gulp.watch(path.build.htmlHome).on('change', browserSync.reload);
-  gulp.watch(path.build.fonts).on('change', browserSync.reload);
-  gulp.watch(path.build.icon, ['sprite:build']);
-  gulp.watch(path.build.img).on('change', browserSync.reload);
-  gulp.watch(path.build.image).on('change', browserSync.reload);
-  gulp.watch(path.build.sass, ['sass:build']);
-	gulp.watch(path.build.jsLib, ['js:build']);
-  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
-  gulp.watch(path.build.js).on('change', browserSync.reload);
-});
-// ===========================================
-gulp.task('watchHtml', function () {
-  gulp.watch(path.build.htmlRiggerFiles, ['html:build']);
-  gulp.watch(path.build.fonts).on('change', browserSync.reload);
-  gulp.watch(path.build.icon, ['sprite:build']);
-  gulp.watch(path.build.img).on('change', browserSync.reload);
-  gulp.watch(path.build.image).on('change', browserSync.reload);
-  gulp.watch(path.build.sass, ['sass:build']);
-	gulp.watch(path.build.jsLib, ['js:build']);
-  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
-  gulp.watch(path.build.js).on('change', browserSync.reload);
+	gulp.watch(path.source.htmlRiggerFiles, ['html:build']);
+	gulp.watch(path.source.htmlHome).on('change', browserSync.reload);
+	gulp.watch(path.source.sass, ['sass:build']);
+	gulp.watch(path.source.sassPath, ['sass:build']);
+	gulp.watch(path.source.jsLib, ['js:build']);
+	gulp.watch(path.source.js).on('change', browserSync.reload);
 });
 
+
 // Gulp task default
-//===========================================
-gulp.task('default', ['sprite:build','sass:build','js:build','jsMain:build','webServer','watch']);
-//===========================================
-gulp.task('html', ['html:build','sprite:build','sass:build','js:build','jsMain:build','webServer','watchHtml']);
+gulp.task('default', [
+	'html:build',
+	'sass:build',
+	'js:build',
+	'webServer',
+	'watch',
+]);
