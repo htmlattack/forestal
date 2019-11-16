8a93e17a5466bc1330427aed398099e0c3e0f75e
alexandroleynik <alexandr@oleynik.org>
2019-11-16 20:30:44 +0200

    Add pages: My info, My bikes, Change password, Others

--- src/js/app.js
+++ src/js/app.js
@@ -22,4 +22,23 @@ $(document).ready(function(){
             prevNextButtons: false,
         });
     }
+
+    var inpdecorate = $('.inp-decorate');
+
+    if(inpdecorate.length) {
+        inpdecorate.styler({
+            
+        });
+    }
+
+
+    // Cart
+    $('.js-cart-more-toggle').on('click', function(e){
+        $this = $(this);
+
+        e.preventDefault();
+
+        $this.closest('.cart-item').find('.cart-item__more').slideToggle(250);
+        $this.text($this.text() == 'Show details' ? 'Hide details' : 'Show details');
+    });
 });
